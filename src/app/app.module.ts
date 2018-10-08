d03545a0190cc4f92dfb52c9feb9fac765759666
William <wrussellbeltran@gmail.com>
2018-10-07 19:09:09 -0600

    Cascaron para proyectos

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
+// Modulos
+import { PageModule } from './pages/pages.module';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    PageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
